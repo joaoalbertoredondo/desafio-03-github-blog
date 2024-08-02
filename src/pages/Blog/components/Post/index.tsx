9b05b9f8e046aabce01e5fa9d9f0b960b29a8613
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-08-02 17:51:35 -0300

    card container created

--- src/pages/Blog/components/Post/index.tsx
+++ src/pages/Blog/components/Post/index.tsx
@@ -1,7 +1,89 @@
-import { PostContainer } from "./styles";
+import { Card, PostContainer } from "./styles";
 
 function Post() {
-  return <PostContainer>Post</PostContainer>;
+  return (
+    <PostContainer>
+      <Card>
+        <header>
+          <h1>JavaScript data types and data structures</h1>
+          <span>Há 1 dia</span>
+        </header>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn. Dynamic typing JavaScript is a loosely typed and dynamic
+          language. Variables in JavaScript are not directly associated with any
+          particular value type, and any variable can be assigned (and
+          re-assigned) values of all types: let foo = 42; // foo is now a number
+          foo = 'bar'; // foo is now a string foo = true; // foo is now a
+          boolean
+        </p>
+      </Card>
+
+      <Card>
+        <header>
+          <h1>JavaScript data types and data structures</h1>
+          <span>Há 1 dia</span>
+        </header>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn. Dynamic typing JavaScript is a loosely typed and dynamic
+          language. Variables in JavaScript are not directly associated with any
+          particular value type, and any variable can be assigned (and
+          re-assigned) values of all types: let foo = 42; // foo is now a number
+          foo = 'bar'; // foo is now a string foo = true; // foo is now a
+          boolean
+        </p>
+      </Card>
+
+      <Card>
+        <header>
+          <h1>JavaScript data types and data structures</h1>
+          <span>Há 1 dia</span>
+        </header>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn. Dynamic typing JavaScript is a loosely typed and dynamic
+          language. Variables in JavaScript are not directly associated with any
+          particular value type, and any variable can be assigned (and
+          re-assigned) values of all types: let foo = 42; // foo is now a number
+          foo = 'bar'; // foo is now a string foo = true; // foo is now a
+          boolean
+        </p>
+      </Card>
+
+      <Card>
+        <header>
+          <h1>JavaScript data types and data structures</h1>
+          <span>Há 1 dia</span>
+        </header>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn. Dynamic typing JavaScript is a loosely typed and dynamic
+          language. Variables in JavaScript are not directly associated with any
+          particular value type, and any variable can be assigned (and
+          re-assigned) values of all types: let foo = 42; // foo is now a number
+          foo = 'bar'; // foo is now a string foo = true; // foo is now a
+          boolean
+        </p>
+      </Card>
+    </PostContainer>
+  );
 }
 
 export default Post;
--- src/pages/Blog/components/Post/styles.ts
+++ src/pages/Blog/components/Post/styles.ts
@@ -1,5 +1,49 @@
 import styled from "styled-components";
 
 export const PostContainer = styled.div`
-margin-top: 48px;
-`
+  margin-top: 48px;
+  margin-bottom: 234px;
+
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 2rem;
+`;
+
+export const Card = styled.div`
+  padding: 2rem;
+  width: 416px;
+  height: 260px;
+
+  border-radius: 10px;
+  background: ${(props) => props.theme["base-post"]};
+
+  header {
+    display: flex;
+    gap: 1rem;
+  }
+
+  h1 {
+    color: ${(props) => props.theme["base-title"]};
+    font-weight: bold;
+    line-height: 160%;
+    font-size: 1.25rem;
+    flex: 1;
+  }
+
+  span {
+    color: ${(props) => props.theme["base-span"]};
+    font-size: 0.875rem;
+    line-height: 160%;
+  }
+
+  p {
+    color: ${(props) => props.theme["base-text"]};
+    margin-top: 1.25rem;
+
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+`;
